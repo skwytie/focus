9f18163862e29b62a831ca122f2c2664fb6810ed
skwytie <98531225+skwytie@users.noreply.github.com>
2024-12-05 14:21:40 +0000

    Initial commit

---
+++ include/header.inc.php
@@ -0,0 +1,66 @@
+<!-- barre de nav -->
+
+<div class="container-fluid">
+      <div class="navb-logo">
+        Logo
+      </div>
+      <div class="navb-items">
+        <div class="item">
+          <a href="/">Home</a>
+        </div>
+        <div class="item">
+          <a href="/about">About</a>
+        </div>
+        <div class="item">
+          <a href="/resume">Resume</a>
+        </div>
+        <div class="item">
+          <a href="/portfolio">Portfolio</a>
+        </div>
+        <div class="item-button">
+          <a href="/contact">Let's Talk</a>
+        </div>
+      </div>
+      <!-- Button Trigger Modal Start -->
+      <div class="mobile-toggle">
+        <a href="#" data-bs-toggle="modal" data-bs-target="#navModal">
+          <i class="bi bi-list"></i>
+        </a>
+      </div>
+      <!-- Modal Trigger Modal End -->
+      <!-- Modal Start -->
+      <div class="modal fade" id="navModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <p>Logo</p>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-x"></i></button>
+            </div>
+            <div class="modal-body">
+              <div class="modal-line">
+                <i class="bi bi-house"></i><a href="/">Home</a>
+              </div>
+              <div class="modal-line">
+                <i class="bi bi-info-circle"></i><a href="/about">About</a>
+              </div>
+              <div class="modal-line">
+                <i class="bi bi-filetype-doc"></i><a href="/resume">Resume</a>
+              </div>
+              <div class="modal-line">
+                <i class="bi bi-folder-check"></i><a href="/portfolio">Portfolio</a>
+              </div>
+              <div class="modal-line">
+                <i class="bi bi-telephone"></i><a href="/contact">Let's Talk</a>
+              </div>
+            </div>
+            <div class="mobile-modal-footer">
+              <a target="_blank" href="#"><i class="bi bi-instagram"></i></a>
+              <a target="_blank" href="#"><i class="bi bi-linkedin"></i></a>
+              <a target="_blank" href="#"><i class="bi bi-youtube"></i></a>
+              <a target="_blank" href="#"><i class="bi bi-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal End -->
+    </div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once('include/head.inc.php'); ?>
+
+    <title>Focus</title>
+</head>
+<body>
+    <header>
+        <?php include('./include/header'); ?>
+    </header>
+</body>
+</html>
